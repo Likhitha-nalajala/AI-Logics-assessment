4b3c9494ede4adaf5f79eb08aa3cd1b7f97fd374
Likhitha-nalajala <likhitha1725@gmail.com>
2026-09-25 13:02:59 +0530

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n = int(input())
+arr = list(map(int, input().split()))
+k = int(input())
+max_dq = deque()  
+min_dq = deque() 
+
+left = 0
+max_length = 0
+best_start = 0
+
+for right in range(n):
+    while max_dq and arr[max_dq[-1]] <= arr[right]:
+        max_dq.pop()
+    max_dq.append(right)
+
+    while min_dq and arr[min_dq[-1]] >= arr[right]:
+        min_dq.pop()
+    min_dq.append(right)
+
+    while arr[max_dq[0]] - arr[min_dq[0]] > k:
+        if max_dq[0] == left:
+            max_dq.popleft()
+
+        if min_dq[0] == left:
+            min_dq.popleft()
+
+        left += 1
+    length = right - left + 1
+    if length > max_length:
+        max_length = length
+        best_start = left
+
+print(max_length, best_start + 1)
